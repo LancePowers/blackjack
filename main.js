41aa334923d0a3197b90a245c5f3c3247dd1cdeb
Lance <Lance@thelpstory.com>
2015-07-16 19:49:23 -0600

    added dom functions for displaying cards. Clean and functioning

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-var deckImage = ["🂡","🂢","🂣","🂤","🂥","🂦","🂧","🂨","🂩","🂪","🂫","🂬","🂭","🂮","🂱","🂲","🂳","🂴","🂵","🂶","🂷","🂸","🂹","🂺","🂻","🂼","🂽","🂾","🃁","🃂","🃃","🃄","🃅","🃆","🃇","🃈","🃉","🃊","🃋","🃌","🃍","🃎","🃑","🃒","🃓","🃔","🃕","🃖","🃗","🃘","🃙","🃚","🃛","🃜","🃝","🃞"];
-//var deckImage = new Array[🂡,🂢,🂣,🂤,🂥,🂦,🂧,🂨,🂩,🂪,🂫,🂬,🂭,🂮,🂱,🂲,🂳,🂴,🂵,🂶,🂷,🂸,🂹,🂺,🂻,🂼,🂽,🂾,🃁,🃂,🃃,🃄,🃅,🃆,🃇,🃈,🃉,🃊,🃋,🃌,🃍,🃎,🃑,🃒,🃓,🃔,🃕,🃖,🃗,🃘,🃙,🃚,🃛,🃜,🃝,🃞];
-console.log(deckImage);
-
-//Create the initial deck *need to figure out how to display unicode
+//Global variable for players hands
+//deckImage is an array of images to use for cards.
+var deckImage =  [
+   '&#x1F0A1','&#x1F0A2','&#x1F0A3','&#x1F0A4','&#x1F0A5','&#x1F0A6','&#x1F0A7',
+   '&#x1F0A8','&#x1F0A9','&#x1F0AA','&#x1F0AB','&#x1F0AD','&#x1F0AE','&#x1F0B1',
+   '&#x1F0B2','&#x1F0B3','&#x1F0B4','&#x1F0B5','&#x1F0B6','&#x1F0B7','&#x1F0B8',
+   '&#x1F0B9','&#x1F0BA','&#x1F0BB','&#x1F0BD','&#x1F0BE','&#x1F0C1','&#x1F0C2',
+   '&#x1F0C3','&#x1F0C4','&#x1F0C5','&#x1F0C6','&#x1F0C7','&#x1F0C8','&#x1F0C9',
+   '&#x1F0CA','&#x1F0CB','&#x1F0CD','&#x1F0CE','&#x1F0D1','&#x1F0D2','&#x1F0D3',
+   '&#x1F0D4','&#x1F0D5','&#x1F0D6','&#x1F0D7','&#x1F0D8','&#x1F0D9','&#x1F0DA',
+   '&#x1F0DB','&#x1F0DD', '&#x1F0DE'
+ ];
+//deckValue is an array of card values
 var deckValue= [
   1,2,3,4,5,6,7,8,9,10,10,10,10,
   1,2,3,4,5,6,7,8,9,10,10,10,10,
   1,2,3,4,5,6,7,8,9,10,10,10,10,
   1,2,3,4,5,6,7,8,9,10,10,10,10
 ];
-
-//deckNames is a function to combine the faces and suits into a diplayable name//
+//deckNames is a function to combine the faces and suits into a diplayable name //it probably isn't necessary.
 function deckNames(){
-  var suit = [" of Hearts"," of Spades"," of Clubs"," of Diamonds"];
+  var suit = [" of Clubs"," of Diamonds"," of Hearts"," of Spades"];
   var face = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"];
   face = face.reverse();
   var size = 52;
@@ -26,7 +33,7 @@ function deckNames(){
   return deckNames;
 }
 
-//runs 2 strings (a third for images) to combine cards in a deck. Can easily expand to other strings of data to add to cards if needed.
+//runs 3 arrays to combine name, value, and image into an array of objects.
 function createDeck(){
   for (var i = 0; i < 52; i++) {
     var name = deckNames()[i+1];
@@ -36,7 +43,7 @@ function createDeck(){
   }
 }
 
-//Builds a card with name, value, and image. Can add a parameter and additional string to the createdDeck.
+//Card builds a card with name, value, and image. Can add a parameter and additional string to the createdDeck.
 //I think this may be able to be combined with createDeck.
 function Card(name,value,image){
   this.name = name;
@@ -65,19 +72,25 @@ function Hand(name) {
   console.log(this.cards);
 }
 
+//prompts user for name and creates 3 hands (2 player 1 dealer).
+//Need to add ability to choose # of players
+function newPlayer(){
+  var p1name = prompt("What's your name?");
+  var player1hand = new Hand(p1name);
+  return player1hand;
+}
+
 //checks a hand for blackJack
 function checkBlackjack(hand){
-if ((hand.cards[0].value + hand.cards[1].value === 11) && (hand.cards[0].value ===10 || hand.cards[1].value === 10) ) {
-  alert(hand.name + " BlackJack");
-} else {
-  console.log("No Blackjack");
+  if ((hand.cards[0].value + hand.cards[1].value === 11) && (hand.cards[0].value ===10 || hand.cards[1].value === 10) ) {
+    alert(hand.name + " BlackJack");
+  } else {
+    console.log("No Blackjack");
+  }
 }
-}
-var player1hand = new Hand('zach')
-var player2hand = new Hand('lance')
-var dealerHand = new Hand('computer')
 
-//function to determine value of a hand/if it's busted/////STILL NEED TO FIGURE OUT SOFT HANDS
+//function to determine value of a hand/if it's busted///
+//STILL NEED TO FIGURE OUT SOFT HANDS
 function cardValue(hand){
   var handValue = 0;
   for (var i = 0; i < hand.cards.length; i++) {
@@ -91,26 +104,35 @@ function cardValue(hand){
   return handValue;
 }
 
-updateDom(player1hand.cards[0].image,"card");
-
-function updateDom(html, id){
+function updateHand(id){
+  var cardPics = "";
+  for (var i = 0; i < player1hand.cards.length; i++) {
+     cardPics = cardPics + player1hand.cards[i].image;
+     console.log(cardPics);
+  }
   var element = document.getElementById(id);
-  element.innerHTML = html;
+  element.innerHTML = cardPics;
 }
-//
-//console.log(player1hand.cards[0].value);
-//console.log(deck.length);
+
+var player1hand = newPlayer();
+var dealerHand = new Hand('Dealer');
+updateHand("card");
 checkBlackjack(player1hand);
-checkBlackjack(player2hand);
 checkBlackjack(dealerHand);
+
+
+
+
 console.log(player1hand);
 console.log(cardValue(player1hand));
+
 var hit = prompt("hit?");
+
 while (hit === "Y"){
   player1hand.cards.push(selectCard()[0]);
+  updateHand("card");
   console.log(cardValue(player1hand));
-  console.log(player1hand);
-  hit = prompt("hit?");
+  hit = prompt("hit? ");
 }
 //
 // console.log("player1 ",player1hand.cards,
@@ -133,3 +155,6 @@ while (hit === "Y"){
 //
 //
 // //for loop on array to test value of hand.
+////////////////////////////Trash//////////////
+// var deckImage = ["🂡","🂢","🂣","🂤","🂥","🂦","🂧","🂨","🂩","🂪","🂫","🂬","🂭","🂮","🂱","🂲","🂳","🂴","🂵","🂶","🂷","🂸","🂹","🂺","🂻","🂼","🂽","🂾","🃁","🃂","🃃","🃄","🃅","🃆","🃇","🃈","🃉","🃊","🃋","🃌","🃍","🃎","🃑","🃒","🃓","🃔","🃕","🃖","🃗","🃘","🃙","🃚","🃛","🃜","🃝","🃞"];
+//var deckImage = new Array[🂡,🂢,🂣,🂤,🂥,🂦,🂧,🂨,🂩,🂪,🂫,🂬,🂭,🂮,🂱,🂲,🂳,🂴,🂵,🂶,🂷,🂸,🂹,🂺,🂻,🂼,🂽,🂾,🃁,🃂,🃃,🃄,🃅,🃆,🃇,🃈,🃉,🃊,🃋,🃌,🃍,🃎,🃑,🃒,🃓,🃔,🃕,🃖,🃗,🃘,🃙,🃚,🃛,🃜,🃝,🃞];
